6516811daf89390b3ab50497145d513562a695a2
Owen Elliot <sgt.owen.elliot@gmail.com>
2024-06-09 03:21:54 -0700

    ok

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const port = process.env.PORT || 3000;
+const path=require('path')
 
 app.get("/",(req,res)=>{
     if(req.headers['user-agent']==="Mozilla/5.0 (Windows NT 861; Win64; x32; rv:621007.0) Gecko/202420232202 Firefox/1616.0"){
-        return res.sendFile("index.html");
+        return res.sendFile(path.join(__dirname,"index.html"));
     }else{
         return res.json({
             message:"Useragent not found"
